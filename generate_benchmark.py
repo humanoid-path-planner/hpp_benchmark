ef683f5d5aa17726734855cb92e561dab1452288
Le Quang Anh <43576719+Toefinder@users.noreply.github.com>
2022-05-16 14:03:02 +0200

    Reset hppcorbaserver for each new benchmark
    Some benchmarks use manipulation plugin, some do not. We should either
    run those that do not use plugin first; or we can just reset
    hppcorbaserver for each new benchmark. The latter option is used to
    ensure more fairness.

--- generate_benchmark.py
+++ generate_benchmark.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys
+from subprocess import Popen
 import shutil
 
 script_name = "script.py"
@@ -10,14 +11,25 @@ def main():
     new_dir_name = sys.argv[1]
     shutil.copytree(benchmark_descr_dir, new_dir_name)
     os.chdir(new_dir_name)
+    # note that "pr2-in-iai-kitchen" and "pyrene-on-the-ground" do not use
+    # the manipulation plugin ("manipulation-corba.so") so we should either
+    # restart hppcorbaserver each time, or to run these two benchmarks before
+    # running the rest which loads the plugin
+
     # get names of all immediate subdirectories that contain a script.py file
     dir_names = [f.name for f in os.scandir(".") if f.is_dir()
                 and os.path.isfile(os.path.join(f.path, script_name))]
 
-    os.system("xterm -e hppcorbaserver &")
     for name in dir_names:
+        # reset the server for every new benchmark
+        server_proc = Popen(["hppcorbaserver"])
         print(f"Executing {name}/script.py")
-        os.system(f"xterm -e 'python3 {name}/script.py > {name}/benchmark 2>&1'")
+        with open(f"{name}/benchmark", "w") as log_file:
+            client_proc = Popen(['python3', f'{name}/script.py'],
+                                stdout=log_file, stderr=log_file)
+            client_proc.wait()
+        server_proc.terminate()
+        server_proc.wait()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
