e41e01143fbab7afab71850165d8f09378ed5109
Florent Lamiraux <florent@laas.fr>
2026-05-06 07:22:56 +0000

    [pr2-manipulation] Use box from this package instead of hpp_tutorial.

--- future/pr2-manipulation-kitchen-spf/script.py
+++ future/pr2-manipulation-kitchen-spf/script.py
@@ -19,8 +19,8 @@ args = parser.parse_args()
 
 pr2_urdf = "package://example-robot-data/robots/pr2_description/urdf/pr2.urdf"
 pr2_srdf = "package://example-robot-data/robots/pr2_description/srdf/pr2_manipulation.srdf"
-box_urdf = "package://hpp_tutorial/urdf/box.urdf"
-box_srdf = "package://hpp_tutorial/srdf/box.srdf"
+box_urdf = "package://hpp_benchmark/urdf/pr2-manipulation-kitchen/box.urdf"
+box_srdf = "package://hpp_benchmark/srdf/pr2-manipulation-kitchen/box.srdf"
 kitchen_urdf = "package://hpp_tutorial/urdf/kitchen_area_obstacle.urdf"
 kitchen_srdf = "package://hpp_tutorial/srdf/kitchen_area.srdf"
 
